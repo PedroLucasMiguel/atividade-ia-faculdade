66fd22a568d91b881ce3b572be20c6522f61d579
Math_RPG <maugusilva.santos@gmail.com>
2023-01-12 13:32:01 -0300

    Implement decision tree documentation example, using defined dataset

--- src/decision-tree/main.py
+++ src/decision-tree/main.py
@@ -1,5 +1,14 @@
+from sklearn.datasets import load_digits
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+
 def main():
-    print('Hello, world!')
+    digits = load_digits()
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(digits.data, digits.target)
+    tree.plot_tree(clf)
+    plt.show()
 
 
 if __name__ == '__main__':
